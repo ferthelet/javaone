53ba23c025000aa28182051f989b27e6d54b6e79
Juan Salazar <ferthelet@gmail.com>
2024-09-16 19:44:51 -0500

    crea PrimeraClase

---
+++ PrimeraClase/src/PrimeraClase.java
@@ -0,0 +1,5 @@
+public class PrimeraClase {
+    public static void main(String[] args) throws Exception {
+        System.out.println("¡Concluí la aula 01 y ahora estoy sumergiéndome en Java!");
+    }
+}
