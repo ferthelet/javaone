07178cf28ba5a1891500885a845a2cae4106f8b7
Juan Salazar <ferthelet@gmail.com>
2024-09-17 22:34:58 -0500

    cambia a Scree Match, variables y media

--- primerprograma/src/App.java
+++ primerprograma/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola Mundo Alura! =)");
+        System.out.println("Bienvenido a Screen MAtch");
+        System.out.println("Pelicula Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (notaDeLaPelicula + 6.0 + 9.0) / 3;
+
+        System.out.println(media);
     }
 }
