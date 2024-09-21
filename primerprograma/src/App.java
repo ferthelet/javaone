c95f61366d3160019cdeb723aa95cf2e4006c943
Juan Salazar <ferthelet@gmail.com>
2024-09-20 19:52:37 -0500

    agrega sinopsis

--- primerprograma/src/App.java
+++ primerprograma/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bienvenido a Screen MAtch");
+        System.out.println("Bienvenido a Screen Match");
         System.out.println("Pelicula Matrix");
 
         int fechaDeLanzamiento = 1999;
@@ -8,7 +8,15 @@ public class App {
         double notaDeLaPelicula = 8.2;
 
         double media = (notaDeLaPelicula + 6.0 + 9.0) / 3;
-
         System.out.println(media);
+
+        String sinopsis = """
+                Matrix es una paradoja
+                La mejor pelicula del fin del milenio
+                Fue lanzada en:
+                """ + fechaDeLanzamiento;
+        System.out.println(sinopsis);
+
+
     }
 }
