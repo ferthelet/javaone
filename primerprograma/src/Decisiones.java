c5c6bf7d0f79ef9997e60b4b52a9b3b4da8d0af0
Juan Salazar <ferthelet@gmail.com>
2024-09-23 21:41:37 -0500

    agrega si pelicula recomendada y incluido en el Plan

--- primerprograma/src/Decisiones.java
+++ primerprograma/src/Decisiones.java
@@ -2,12 +2,19 @@ public class Decisiones {
     public static void main(String[] args) {
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnElPlan = true;
+        String tipoPlan = "plus";
         double notaDeLaPelicula = 8.2;
 
         if (fechaDeLanzamiento >= 2022) {
             System.out.println("Peliculas más populares");
         } else {
             System.out.println("Pelicula retro");
+        }
+        if (incluidoEnElPlan && notaDeLaPelicula >= 8) {
+            System.out.println("Pelicula recomendada");
+        }
+        if (incluidoEnElPlan && tipoPlan.equals("plus")) {
+            System.out.println("Disfrute de su pelicula");
         }
     }
 }
