0817c5cb0d7dbdeb8a067787f4e7a9e37a9641a6
Juan Salazar <ferthelet@gmail.com>
2024-09-27 17:50:57 -0500

    primer commit

---
+++ AplicacionBancaria/src/AplicacionBancaria.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class AplicacionBancaria {
+    private static double saldo = 1599.0;
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        int opcion;
+        do {
+            mostrarMenu();
+            System.out.println("Por favor, ingrese un número válido.");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    consultarSaldo();
+                    break;
+                case 2:
+                    retirar();
+                    break;
+                case 3:
+                    depositar();
+                    break;
+                case 4:
+                    System.out.println("Gracias por usar nuestros servicios. Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Opción inválida. Inténtelo de nuevo.");
+            }
+        } while (opcion != 4);
+    }
+
+    public static void mostrarMenu() {
+        System.out.println("\n--- Menú de Operaciones Bancarias ---");
+        System.out.println("1. Consultar saldo");
+        System.out.println("2. Retirar");
+        System.out.println("3. Depositar");
+        System.out.println("4. Salir");
+        System.out.print("Seleccione una opción: ");
+    }
+
+    public static void consultarSaldo() {
+        System.out.printf("Su saldo actual es: $%.2f\n", saldo);
+    }
+}
