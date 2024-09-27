b5cf1be9ba7ca33a3a35206e72464ae5b611e367
Juan Salazar <ferthelet@gmail.com>
2024-09-27 16:47:26 -0500

    corrigge error de directorios

---
+++ adivinanza/src/JuegoAdivinanzas.java
@@ -0,0 +1,10 @@
+public class JuegoAdivinanzas {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Bievenido al juego de adivinanza!");
+        jugarAdivinanzas();
+    }
+
+    public static void jugarAdivinanzas() {
+
+    }
+}
