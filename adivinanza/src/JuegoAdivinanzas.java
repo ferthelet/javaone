be242252cc090e12c79120cff85643cdb929f476
Juan Salazar <ferthelet@gmail.com>
2024-09-27 17:14:59 -0500

    agrega variables a usar

--- adivinanza/src/JuegoAdivinanzas.java
+++ adivinanza/src/JuegoAdivinanzas.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class JuegoAdivinanzas {
     public static void main(String[] args) throws Exception {
         System.out.println("Bievenido al juego de adivinanza!");
@@ -5,6 +8,10 @@ public class JuegoAdivinanzas {
     }
 
     public static void jugarAdivinanzas() {
+        Random random = new Random();
+        int numeroSecreto = random.nextInt(101); // Genera un número entre 0 y 100
+        int intentosMaximos = 5;
+        Scanner scanner = new Scanner(System.in);
 
     }
 }
