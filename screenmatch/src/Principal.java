dce6af266dd7f6aa813a21f348a33f3dba71b64e
Juan Salazar <ferthelet@gmail.com>
2024-09-28 13:44:27 -0500

    agrega instancia otraPelicula; imprime todos los atributos

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -8,5 +8,18 @@ public class Principal {
 
         System.out.println("Mi pelicula es: " + miPelicula.nombre);
         System.out.println("Su fecha de lanzamiento es: " + miPelicula.fechaDeLanzamiento);
+        System.out.println("Su duracion en minutos es: " + miPelicula.duracionEnMinutos);
+        System.out.println("Esta incluida en el plan: " + miPelicula.incluidoEnElplan);
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "Matrix";
+        otraPelicula.fechaDeLanzamiento = 1999;
+        otraPelicula.duracionEnMinutos = 180;
+        otraPelicula.incluidoEnElplan = false;
+
+        System.out.println("Mi otra pelicula es: " + otraPelicula.nombre);
+        System.out.println("Su fecha de lanzamiento es: " + otraPelicula.fechaDeLanzamiento);
+        System.out.println("Su duracion en minutos es: " + otraPelicula.duracionEnMinutos);
+        System.out.println("Esta incluida en el plan: " + otraPelicula.incluidoEnElplan);
     }
 }
