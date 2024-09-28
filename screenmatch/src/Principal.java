815d9bd908aa5df7c01a64223f70247a15760106
Juan Salazar <ferthelet@gmail.com>
2024-09-28 13:36:07 -0500

    crea Principal y primera pelicula

---
+++ screenmatch/src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.nombre = "Encanto";
+        miPelicula.fechaDeLanzamiento = 2021;
+        miPelicula.duracionEnMinutos = 120;
+        miPelicula.incluidoEnElplan = true;
+    }
+}
