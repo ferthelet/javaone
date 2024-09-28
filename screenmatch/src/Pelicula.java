fd007ea30e46d1ec218a2a460cdc8235b0be76a8
Juan Salazar <ferthelet@gmail.com>
2024-09-28 13:30:59 -0500

    crea proyecto y define varisables iniciales

---
+++ screenmatch/src/Pelicula.java
@@ -0,0 +1,10 @@
+public class Pelicula {
+
+    String nombre;
+    int fechaDeLanzamiento;
+    int duracionEnMinutos;
+    boolean incluidoEnElplan;
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Pelicula!");
+    }
+}
