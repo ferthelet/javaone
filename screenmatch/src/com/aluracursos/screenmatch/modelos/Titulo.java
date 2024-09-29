459935e54ed38eac56fa81a79bb097a852376e34
Juan Salazar <ferthelet@gmail.com>
2024-09-28 19:00:27 -0500

    reemplaza atributo duracionEnMinutos por getDuracionEnMinutos

--- screenmatch/src/com/aluracursos/screenmatch/modelos/Titulo.java
+++ screenmatch/src/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -46,7 +46,7 @@ public class Titulo {
     public void muestraFichaTecnica() {
         System.out.println("Nombre de la pelicula: " + nombre);
         System.out.println("Fecha de lanzamiento: " + fechaDeLanzamiento);
-        System.out.println("Duracion en minutos: " + duracionEnMinutos);
+        System.out.println("Duracion en minutos: " + getDuracionEnMinutos());
         System.out.println("Incluida en el plan: " + incluidoEnElplan);
     }
 
