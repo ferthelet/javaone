2f059110e781038fc99031e798c300a3e5d44fe8
Juan Salazar <ferthelet@gmail.com>
2024-09-28 19:43:45 -0500

    agrega getter para tiempoTotal

--- screenmatch/src/com/aluracursos/screenmatch/calculos/calculadoraDeTiempo.java
+++ screenmatch/src/com/aluracursos/screenmatch/calculos/calculadoraDeTiempo.java
@@ -4,8 +4,15 @@ import com.aluracursos.screenmatch.modelos.Pelicula;
 public class CalculadoraDeTiempo {
 
     private int tiempoTotal;
+    
+    // getter 
+    public int getTiempoTotal() {
+        return tiempoTotal;
+    }
 
     public void incluye(Pelicula pelicula) {
         tiempoTotal += pelicula.getDuracionEnMinutos();
     }
+
+
 }
