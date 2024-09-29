f1f7515655d43d08c039d9df9fa9f96521809c89
Salazar <slzrn@amazon.com>
2024-09-29 16:38:20 -0500

    crea clase Canciones, atributos y sets/gets

---
+++ radioalura/src/com/aluracursos/radioalura/modelos/Canciones.java
@@ -0,0 +1,29 @@
+package com.aluracursos.radioalura.modelos;
+
+public class Canciones {
+
+    private String album;
+    private String cantante;
+    private String genero;
+
+    // getters y setters
+    public String getAlbum() {
+        return album;
+    }
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+    public String getCantante() {
+        return cantante;
+    }
+    public void setCantante(String cantante) {
+        this.cantante = cantante;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+}
