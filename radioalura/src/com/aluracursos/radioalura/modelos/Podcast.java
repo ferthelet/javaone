1a77d73b486f0274c0edc0fd62cf2b555b32f3c2
Juan Salazar <ferthelet@gmail.com>
2024-09-29 19:47:15 -0500

    sobreescribe metodo getClasificacion en Podcast

--- radioalura/src/com/aluracursos/radioalura/modelos/Podcast.java
+++ radioalura/src/com/aluracursos/radioalura/modelos/Podcast.java
@@ -5,6 +5,15 @@ public class Podcast extends Audio {
     private String presentador;
     private String descripcion;
 
+    @Override
+    public int getClasificacion() {
+        if (getTotalReproducciones() >= 2000) {
+            return 9;
+        } else {
+            return 2;
+        }
+    }
+
     // getters y setters
     public String getPresentador() {
         return presentador;
