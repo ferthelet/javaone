29088d81a8af3514315d3e2f8f6b1caefd28643e
Juan Salazar <ferthelet@gmail.com>
2024-09-29 19:55:04 -0500

    hace public agregar()

--- radioalura/src/com/aluracursos/radioalura/modelos/MisFavoritos.java
+++ radioalura/src/com/aluracursos/radioalura/modelos/MisFavoritos.java
@@ -2,7 +2,7 @@ package com.aluracursos.radioalura.modelos;
 
 public class MisFavoritos {
 
-    void agregar(Audio audio) {
+    public void agregar(Audio audio) {
         if (audio.getClasificacion() >= 8) {
             System.out.println(audio.getTitulo() + " es uno de los favoritos del momento");
         } else {
