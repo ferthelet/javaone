3d4490ab1a5641555b331195dbaafcaffa4bf311
Juan Salazar <ferthelet@gmail.com>
2024-10-04 13:53:01 -0500

    agrega casting directo con Animal myAnimal = new Dog()

--- exercises/casting/src/Casting.java
+++ exercises/casting/src/Casting.java
@@ -2,5 +2,8 @@ public class Casting {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Casting!");
 
+        // casting directly
+        Animal myAnimal = new Dog();
+        myAnimal.makeNoise();
     }
 }
