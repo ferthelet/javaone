b072ebbf5e0732679395c546e93e7b223822d7f1
Juan Salazar <ferthelet@gmail.com>
2024-10-04 17:28:59 -0500

    agrega scanner, limite, e intenta lista de compra

--- Compras/src/RegistroDeCompras.java
+++ Compras/src/RegistroDeCompras.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class RegistroDeCompras {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Lista de Compras!");
@@ -7,10 +11,29 @@ public class RegistroDeCompras {
         // Menu para registrar compras
         // Exhibir lista de compras ordenadas ascendente por precio
 
-        
+        Scanner scanner = new Scanner(System.in);
+        int limiteDeGasto;
+        List<Compra> compras = new ArrayList<>();
 
+                
 
-        
+    }
+}
 
+class Compra {
+    private String nombre;
+    private double precio;
+
+    public Compra(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
     }
 }
