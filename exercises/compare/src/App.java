af58a0a3fe9a8c91d2a5cc8b9b3e0ce16caf0eb6
Juan Salazar <ferthelet@gmail.com>
2024-10-04 12:59:56 -0500

    crea practica comparable/comparator

---
+++ exercises/compare/src/App.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Compares!");
+
+        List<Integer> nums = new ArrayList<Integer>();
+        nums.add(3);
+        nums.add(1);
+        nums.add(4);
+        nums.add(2);
+
+        System.out.println(nums);
+
+    }
+}
