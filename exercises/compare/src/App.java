ed5637bca52e550f0a0d6bf2ec21f174500d7c76
Juan Salazar <ferthelet@gmail.com>
2024-10-04 13:15:11 -0500

    agrega definicion de comparador y usa Collections sort al final

--- exercises/compare/src/App.java
+++ exercises/compare/src/App.java
@@ -1,10 +1,18 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Compares!");
+
+        // using comparator Collcetions below **
+        Comparator<Integer> com = new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                return a % 10 - b % 10;
+            }
+        };
 
         List<Integer> nums = new ArrayList<Integer>();
         nums.add(23);
@@ -22,9 +30,12 @@ public class App {
         // Collections.sort(nums);
 
         // sort by the last digit
-        Collections.sort(nums, (a, b) -> {
-            return a % 10 - b % 10;
-        });
+        // Collections.sort(nums, (a, b) -> {
+        //     return a % 10 - b % 10;
+        // });
+
+        // using comparator above **
+        Collections.sort(nums, com);
 
         System.out.println(nums);
 
