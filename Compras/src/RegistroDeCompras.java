4b6d8af2ad4db7f2675398a33ff4cbcecc5b1c04
Juan Salazar <ferthelet@gmail.com>
2024-10-04 17:22:29 -0500

    crea proyecto y delinea los requerimientos

---
+++ Compras/src/RegistroDeCompras.java
@@ -0,0 +1,16 @@
+public class RegistroDeCompras {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Lista de Compras!");
+        System.out.println("========================");
+
+        // Crear una app para registrar compras en una tarjeta de credito
+        // Menu para registrar compras
+        // Exhibir lista de compras ordenadas ascendente por precio
+
+        
+
+
+        
+
+    }
+}
