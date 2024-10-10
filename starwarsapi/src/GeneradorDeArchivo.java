57d7095e22a7e340ab33c38242251d8c45c7e983
Juan Salazar <ferthelet@gmail.com>
2024-10-10 09:04:42 -0500

    agrega io exception a la firma para generar error

--- starwarsapi/src/GeneradorDeArchivo.java
+++ starwarsapi/src/GeneradorDeArchivo.java
@@ -1,14 +1,14 @@
 import com.google.gson.GsonBuilder;
 import com.google.gson.Gson;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class GeneradorDeArchivo {
 
-    public void guardarJson(Pelicula pelicula) {
+    public void guardarJson(Pelicula pelicula) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         FileWriter escritura = new FileWriter(pelicula.title() + ".json");
         escritura.write(gson.toJson(pelicula));
         escritura.close();
     }
-
 }
