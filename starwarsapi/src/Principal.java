58934925178579a84134fd3b593217d3a45fbdb5
Juan Salazar <ferthelet@gmail.com>
2024-10-10 08:57:08 -0500

    agrega exception para manejo de strings

--- starwarsapi/src/Principal.java
+++ starwarsapi/src/Principal.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
         ConsultaPelicula consultaPelicula = new ConsultaPelicula();
 
+        System.out.println("Escriba el numero de pelicula: ");
+
         try {
-            Pelicula pelicula = consultaPelicula.buscaPelicula(1);
+            int numeroDePelicula = Integer.valueOf(lectura.nextLine());
+            Pelicula pelicula = consultaPelicula.buscaPelicula(numeroDePelicula);
             System.out.println(pelicula);
+        } catch (NumberFormatException e) {
+            System.out.println("Numero no encontrado.");
         } catch (RuntimeException e) {
-            // TODO Auto-generated catch block
             System.out.println(e.getMessage());
             System.out.println("Finalizando la aplicacion.");
         }
