a253322fd278eaed2064babc1eef7daa3b8daf81
Juan Salazar <ferthelet@gmail.com>
2024-10-09 19:00:36 -0500

    crea proyecto starwarsapi, clase Principal, record Pelicula

---
+++ starwarsapi/src/Pelicula.java
@@ -0,0 +1,10 @@
+// star wars api
+public record Pelicula(String title,
+                       int episode_id,
+                       String opening_crawl,
+                       String director,
+                       String producer,
+                       String release_date) {
+    
+
+}
